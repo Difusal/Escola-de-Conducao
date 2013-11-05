9bfb988af90f7b67f6f6420fcb7030ac6266d8d6
Henrique Ferrolho <henriqueferrolho@gmail.com>
2013-11-05 19:26:46 +0000

    restrucured working folder

---
+++ Escola de Conducao/src/main.cpp
@@ -0,0 +1,14 @@
+#include "Escola.h"
+#include "Viatura.h"
+#include "Ligeiro.h"
+#include "Pesado.h"
+#include "Motociclo.h"
+
+using namespace std;
+
+int main(){
+	Escola Viriato;
+	Viriato.showMainMenu();
+
+    return 0;
+}
